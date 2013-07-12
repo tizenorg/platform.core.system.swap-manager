42b1d71e0478d390c12d41bb8eadb940ebaef3a3
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2013-07-12 16:05:57 +0400

    [FIX] ioctl send message fix
     - ioctl defines changed
     - ioctl call fixed

--- daemon/ioctl_commands.c
+++ daemon/ioctl_commands.c
@@ -39,7 +39,7 @@
 int ioctl_send_msg(struct msg_data_t *msg)
 {
 	LOGI("write to device\n");
-	if (ioctl(manager.buf_fd, 0, msg) == -1) {
+	if (ioctl(manager.buf_fd, SWAP_DRIVER_MSG, msg) == -1) {
 		LOGE("write to device: %s\n", strerror(errno));
 		return 1;
 	}
--- daemon/ioctl_commands.h
+++ daemon/ioctl_commands.h
@@ -32,8 +32,8 @@
 
 #include <linux/ioctl.h>
 
-#ifndef __SWAP_DRIVER_IOCTL_COMMANDS__
-#define __SWAP_DRIVER_IOCTL_COMMANDS__
+#ifndef __SWAP_IOCTL_H__
+#define __SWAP_IOCTL_H__
 
 #define SWAP_DRIVER_IOC_MAGIC 0xAF
 
@@ -43,21 +43,16 @@ struct buffer_initialize {
     size_t size;
     unsigned int count;
 };
+#define SWAP_DRIVER_BUFFER_INITIALIZE		_IOW(SWAP_DRIVER_IOC_MAGIC, 1, \
+										     struct buffer_initialize *)
+#define SWAP_DRIVER_BUFFER_UNINITIALIZE		_IO(SWAP_DRIVER_IOC_MAGIC, 2)
+#define SWAP_DRIVER_NEXT_BUFFER_TO_READ		_IO(SWAP_DRIVER_IOC_MAGIC, 3)
+#define SWAP_DRIVER_FLUSH_BUFFER			_IO(SWAP_DRIVER_IOC_MAGIC, 4)
+#define SWAP_DRIVER_MSG						_IOW(SWAP_DRIVER_IOC_MAGIC, 5, \
+										     void *)
 
-#define SWAP_DRIVER_BUFFER_INITIALIZE       _IOW(SWAP_DRIVER_IOC_MAGIC, 1, \
-                                                 struct buffer_initialize *)
-#define SWAP_DRIVER_BUFFER_UNINITIALIZE     _IO(SWAP_DRIVER_IOC_MAGIC, 2)
-#define SWAP_DRIVER_NEXT_BUFFER_TO_READ     _IO(SWAP_DRIVER_IOC_MAGIC, 3)
-#define SWAP_DRIVER_FLUSH_BUFFER            _IO(SWAP_DRIVER_IOC_MAGIC, 4)
-#define SWAP_DRIVER_MSG_START               _IOW(SWAP_DRIVER_IOC_MAGIC, 5, \
-                                                 void *)
-#define SWAP_DRIVER_MSG_STOP                _IO(SWAP_DRIVER_IOC_MAGIC, 6)
-#define SWAP_DRIVER_MSG_CONFIG              _IOW(SWAP_DRIVER_IOC_MAGIC, 7,\
-                                                 void *)
-#define SWAP_DRIVER_MSG_SWAP_INST_ADD       _IOW(SWAP_DRIVER_IOC_MAGIC, 8,\
-                                                 void *)
-#define SWAP_DRIVER_MSG_SWAP_INST_REMOVE    _IOW(SWAP_DRIVER_IOC_MAGIC, 9,\
-                                                 void *)
-extern int ioctl_send_msg(struct msg_data_t *msg);
 
-#endif /* __SWAP_DRIVER_IOCTL_COMMANDS__ */
+int ioctl_send_msg(struct msg_data_t *msg);
+
+
+#endif /* __SWAP_IOCTL_H__ */
