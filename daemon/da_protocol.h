1f2d86b590abf10f9c1681498282c877b2cd10db
Alexander Aksenov <a.aksenov@samsung.com>
2014-03-26 16:44:19 +0400

    [IMPROVE] Implement different kind of probes
    Now probe type is transferred right after the probe address, before the probe's
    data.
    Supported probe types:
     0 - retprobe
     1 - function body instrumentation probe
    
    Change-Id: Ie309e3f911619f3a1b4c821f1ad17932ce9f1c4d
    Signed-off-by: Alexander Aksenov <a.aksenov@samsung.com>

--- daemon/da_protocol.h
+++ daemon/da_protocol.h
@@ -147,6 +147,12 @@ enum feature_code{
 
 };
 
+enum probe_type {
+	SWAP_RETPROBE   = 0, //Common retprobe
+	SWAP_FBI_PROBE  = 1,  //Function body instrumentation probe
+	SWAP_LD_PROBE  = 2  //Function body instrumentation probe
+};
+
 #define IS_OPT_SET_IN(OPT, reg) (reg & (OPT))
 #define IS_OPT_SET(OPT) IS_OPT_SET_IN((OPT), prof_session.conf.use_features0)
 
@@ -240,11 +246,11 @@ struct us_func_inst_plane_t {
 	//name       | type   | len       | info
 	//------------------------------------------
 	//func_addr  | uint64 | 8         |
-	//args       | string | len(args) |end with '\0'
-	//ret_type   | char   | 1         |
+	//probe_type | char   | 1         |
 	uint64_t func_addr;
-	char args[0];
-};
+	char probe_type;
+	char probe_info[0];
+} __attribute__ ((packed));
 
 struct us_lib_inst_t {
 	char *bin_path;
