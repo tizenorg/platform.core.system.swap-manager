d6da4bdcbb07b13d8cd8feef661ac8c982c8b60f
Alexander Aksenov <a.aksenov@samsung.com>
2014-09-10 17:16:26 +0400

    [FEATURE] Add preload modules support
    Change-Id: I12ed37f551eebba6aeb2b492899aa0877d47d625
    Signed-off-by: Alexander Aksenov <a.aksenov@samsung.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,6 +14,8 @@ if [ ! -e /sys/kernel/debug/swap/writer/raw ]; then
     insmod swap_energy.ko || exit 1
     insmod swap_message_parser.ko || exit 1  # parser is loaded
     insmod swap_retprobe.ko || exit 1 # retprobe is loaded
+    insmod swap_task_data.ko || exit 1
+    insmod swap_preload.ko || exit 1
 
 fi
 
