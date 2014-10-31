c3fe8de5011414cdd53e36a6995abe4c12645e3c
Alexander Aksenov <a.aksenov@samsung.com>
2014-10-31 15:23:22 +0300

    [IMPROVE] Add data to be sent to modules
    Add handlers path, linker path, getting r_debug address for passing to the
    modules via debugfs.
    
    Change-Id: I5e18bcc4677800b5f3a9b7f53a1b9d9933a12303
    Signed-off-by: Alexander Aksenov <a.aksenov@samsung.com>

--- scripts/gen_preload_header.sh
+++ scripts/gen_preload_header.sh
@@ -3,6 +3,9 @@ preload_library_name="libdl-2.13.so"
 preload_library_path="/lib/"
 preload_open_function="dlopen@@GLIBC"
 var_addr_file="/usr/local/include/var_addr"
+handlers_lib="/usr/lib/da_probe_tizen.so"
+linker_path="/lib/ld-2.13.so"
+linker_sym="_r_debug"
 
 output=$1
 
@@ -35,6 +38,25 @@ function print_var_offset()
     echo -e "    echo 0x$variabe > /sys/kernel/debug/swap/preload/var_offset" >> $filename
 }
 
+function print_probe_lib()
+{
+    filename=$1
+    echo -e "    echo \"$handlers_lib\" > /sys/kernel/debug/swap/preload/handlers_path" >> $filename
+}
+
+function print_linker_path()
+{
+    filename=$1
+    echo -e "    echo \"$linker_path\" > /sys/kernel/debug/swap/preload/linker/linker_path" >> $filename
+}
+
+function print_r_debug_offset()
+{
+    filename=$1
+    offset=$2
+    echo -e "    echo 0x$offset > /sys/kernel/debug/swap/preload/linker/r_debug_offset" >> $filename
+}
+
 function print_post()
 {
     filename=$1
@@ -46,9 +68,13 @@ function print_post()
 
 open_addr=$(readelf -sW $preload_library_path$preload_library_name | grep $preload_open_function | awk '{print $2}')
 var_offset=$(cat $var_addr_file)
+r_debug_offset=$(readelf -sW $linker_path | grep $linker_sym | awk '{print $2}' | uniq)
 
 print_header $output
 print_lib_name $output
 print_open_function $output $open_addr
 print_var_offset $output $var_offset
+print_probe_lib $output
+print_linker_path $output
+print_r_debug_offset $output $r_debug_offset
 print_post $output
