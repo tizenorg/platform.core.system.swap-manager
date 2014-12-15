43e76a00fceb3f8036767c5138067a6c4de3e767
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2014-12-15 12:03:20 +0300

    [FIX] MSG_BINARY_INFO daemon crash if file type is not ELF
    Change-Id: I5f967f6fe95a541868e360e1dd4b520fbf7d1074
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- daemon/da_protocol.c
+++ daemon/da_protocol.c
@@ -838,7 +838,8 @@ static struct binary_ack* binary_ack_alloc(const char *filename)
 	if (stat(filename, &decoy) == 0) {
 		ba->type = get_binary_type(filename);
 
-		get_build_dir(builddir, filename);
+		if (ba->type != BINARY_TYPE_UNKNOWN)
+			get_build_dir(builddir, filename);
 
 		if (builddir[0] != '\0')
 			snprintf(binpath, sizeof(binpath), check_windows_path(builddir) ?
@@ -883,7 +884,11 @@ static int process_msg_binary_info(struct msg_buf_t *msg)
 		if (new->type == BINARY_TYPE_FILE_NOT_EXIST) {
 			error_code = ERR_WRONG_MESSAGE_DATA;
 			LOGW("binary file not exists <%s>\n", str);
+		} else if (new->type == BINARY_TYPE_UNKNOWN) {
+			error_code = ERR_WRONG_MESSAGE_DATA;
+			LOGW("binary is not ELF binary <%s>\n", str);
 		}
+
 		if (new->binpath[0] == '\0')
 			LOGW("section '.debug_str' not found in <%s>\n", str);
 		acks[i] = new;
