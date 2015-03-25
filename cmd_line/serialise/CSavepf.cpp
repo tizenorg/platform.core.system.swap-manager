0add7000cc84755f456efe0bdd0e673176dfbcd6
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2015-03-25 12:31:21 +0300

    [FEATURE] cli: imlement serialize visitor
    Change-Id: Ifa493a8cb7fd8aee8175e26ad11a826dbff931a5
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- cmd_line/serialise/CSavepf.cpp
+++ cmd_line/serialise/CSavepf.cpp
@@ -1,11 +1,12 @@
 #include "CVisitor.h"
+#include "CSavepf.h"
 
 #include "CProbeData.h"
 #include "CProbe.h"
 #include "CFeatures.h"
 
 /* CFeatures */
-void CSerialise::entry(CFeatures *const features)
+void CSavepf::entry(CFeatures *const features)
 {
 	m_Out << "# Features list\n";
 
@@ -20,29 +21,29 @@ void CSerialise::entry(CFeatures *const features)
 	}
 };
 
-void CSerialise::exit(CFeatures* const features)
+void CSavepf::exit(CFeatures* const features)
 {
 	m_Out << "# CFeatures<<\n";
 };
 
 /* CProbeData */
-void CSerialise::entry(CProbeData* const probe_data)
+void CSavepf::entry(CProbeData* const probe_data)
 {
 	m_Out << "# CProbeData>>\n";
 };
 
-void CSerialise::exit(CProbeData* const probe_data)
+void CSavepf::exit(CProbeData* const probe_data)
 {
 	m_Out << "# CProbeData<<\n";
 };
 
 /* CProbeListElm */
-void CSerialise::entry(CProbeListElm* const probe_list)
+void CSavepf::entry(CProbeListElm* const probe_list)
 {
 	m_Out << "# CProbeListElm>>\n";
 };
 
-void CSerialise::exit(CProbeListElm* const probe_list)
+void CSavepf::exit(CProbeListElm* const probe_list)
 {
 	m_Out << "# CProbeListElm<<\n";
 };
