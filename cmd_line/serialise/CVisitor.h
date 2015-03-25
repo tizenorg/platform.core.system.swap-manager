cecf190a0043e9badb21978539961faba376f9ad
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2015-03-25 12:31:21 +0300

    [FEATURE] cli: imlement serialise visitor
    Change-Id: Ifa493a8cb7fd8aee8175e26ad11a826dbff931a5
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- cmd_line/serialise/CVisitor.h
+++ cmd_line/serialise/CVisitor.h
@@ -80,51 +80,6 @@ class CVisitor
 		virtual ~CVisitor() {};
 };
 
-#define SERIALISER_INTERFACES(CLASS, VAR_NAME) \
-		virtual void entry(CLASS *const VAR_NAME);\
-		virtual void exit(CLASS *const VAR_NAME)
-
-class OutPut
-{
-	public:
-		friend OutPut &operator<<(OutPut &out,const char *st)
-		{
-			printf("%s", st);
-			return out;
-		}
-
-		friend OutPut &operator<<(OutPut &out,std::string st)
-		{
-			out << st.c_str();
-			return out;
-		}
-
-		OutPut() {};
-		virtual ~OutPut() {};
-};
-
-class CSerialise: public CVisitor
-{
-	private:
-		OutPut m_Out;
-	public:
-
-/* CFeatures */
-		SERIALISER_INTERFACES(CFeatures, features);
-/* CProbeData */
-		SERIALISER_INTERFACES(CProbeData, probe_data);
-/* CProbeListElm */
-		SERIALISER_INTERFACES(CProbeListElm, probe_list);
-
-		CSerialise()
-		{
-		}
-
-		virtual ~CSerialise()
-		{
-		}
-};
-
 class CNode
 {
 	public:
