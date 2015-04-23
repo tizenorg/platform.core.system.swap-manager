0b6291dca885a1892bcd3d1457e08b530c1c3daa
Anastasia Lyupa <a.lyupa@samsung.com>
2015-04-23 16:30:42 +0300

    [FEATURE] set webprobe profiling
    - add webkit2-efl-debuginfo dependency to
      determine libewebkit addresses required
      for web app profiling
    - send addresses to modules through debugfs
    
    Change-Id: I75d4e952163db4845de737da42a7152d0b51bd31
    Signed-off-by: Anastasia Lyupa <a.lyupa@samsung.com>

--- daemon/wsi.h
+++ daemon/wsi.h
@@ -28,6 +28,9 @@
 #ifndef WSI_H_
 #define WSI_H_
 
+#include "da_inst.h"
+
+void wsi_set_profile(const struct app_info_t *app_info);
 int wsi_init(const char *address, int port);
 int wsi_start_profiling(void);
 void wsi_destroy(void);
