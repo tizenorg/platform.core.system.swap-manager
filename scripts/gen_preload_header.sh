cfab7c6541ed24f336734d4efccc301865cc45b0
Alexander Aksenov <a.aksenov@samsung.com>
2015-05-25 12:21:41 +0300

    [IMPROVE] Preload: flexible initialization
    Change-Id: Idedb744eed4f54c375a6b7194b2b2a06e9dfbe96
    Signed-off-by: Alexander Aksenov <a.aksenov@samsung.com>

--- scripts/gen_preload_header.sh
+++ scripts/gen_preload_header.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-preload_library_name="libdl-2.13.so"
+preload_library_pattern="libdl[.-].*"
 preload_library_path="/lib/"
 preload_open_function="dlopen@@GLIBC"
 handlers_lib="/usr/lib/da_probe_tizen.so"
-linker_path="/lib/ld-2.13.so"
+linker_path="/lib/"
+linker_pattern="ld[.-].*"
 linker_sym="_r_debug"
 
 output=$1
@@ -12,59 +13,44 @@ output=$1
 function print_header()
 {
     filename=$1
-    echo -e "# Preload\n"\
-            "if [ -d /sys/kernel/debug/swap/preload/ ] \n"\
-            "then" >> $filename
+    echo -e "#!/bin/bash\n"\
+            "#Preload\n" > $filename
 }
 
-function print_open_function()
+function print_loader()
 {
     filename=$1
-    addr=$2
-    echo -e "    echo 0x$addr > /sys/kernel/debug/swap/preload/loader/loader_offset" >> $filename
-}
+    el=$(find $preload_library_path -regextype posix-extended -regex $preload_library_path$preload_library_pattern | head -n1)
+    preload_lib=$(readlink -f $el)
+    addr=$(readelf -sW $preload_lib | grep $preload_open_function | head -1 | awk '{print $2}')
 
-function print_lib_name()
-{
-    filename=$1
-    echo -e "    echo \"$preload_library_path$preload_library_name\" > /sys/kernel/debug/swap/preload/loader/loader_path" >> $filename
+    echo -e "echo \"$preload_lib\" > /sys/kernel/debug/swap/preload/loader/loader_path" >> $filename
+    echo -e "echo 0x$addr > /sys/kernel/debug/swap/preload/loader/loader_offset" >> $filename
 }
 
 function print_probe_lib()
 {
     filename=$1
-    echo -e "    echo \"$handlers_lib\" > /sys/kernel/debug/swap/preload/handlers_path" >> $filename
+    echo -e "echo \"$handlers_lib\" > /sys/kernel/debug/swap/preload/handlers_path" >> $filename
 }
 
-function print_linker_path()
+function print_linker()
 {
     filename=$1
-    echo -e "    echo \"$linker_path\" > /sys/kernel/debug/swap/preload/linker/linker_path" >> $filename
+    el=$(readelf -p .interp /usr/bin/gcc | grep "/lib/" | awk '{print $3}')
+    linker=$(readlink -f $el)
+    r_debug_offset=$(readelf -sW $linker | grep $linker_sym | awk '{print $2}' | uniq)
+
+    echo -e "echo \"$linker\" > /sys/kernel/debug/swap/preload/linker/linker_path" >> $filename
+    echo -e "echo 0x$r_debug_offset > /sys/kernel/debug/swap/preload/linker/r_debug_offset" >> $filename
 }
 
-function print_r_debug_offset()
-{
-    filename=$1
-    offset=$2
-    echo -e "    echo 0x$offset > /sys/kernel/debug/swap/preload/linker/r_debug_offset" >> $filename
-}
-
-function print_post()
-{
-    filename=$1
-    echo -e "fi" >> $filename
-}
 ##################################
 #       Script entry point       #
 ##################################
 
-open_addr=$(readelf -sW $preload_library_path$preload_library_name | grep $preload_open_function | awk '{print $2}')
-r_debug_offset=$(readelf -sW $linker_path | grep $linker_sym | awk '{print $2}' | uniq)
 
 print_header $output
-print_lib_name $output
-print_open_function $output $open_addr
+print_loader $output
 print_probe_lib $output
-print_linker_path $output
-print_r_debug_offset $output $r_debug_offset
-print_post $output
+print_linker $output
