a188067d72da6d2006274eafb02e4554d7a12845
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2015-05-26 17:19:05 +0300

    [FIX] MSG_VERSION ack
    fix wrong ack code
    fix debug MSG_VERSION
    
    Change-Id: I256fd146dfb1a71c08b1893a4294174a6595eab8
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

---
+++ daemon/da_msg_ids.h
@@ -0,0 +1,53 @@
+/*
+ *  DA manager
+ *
+ * Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact:
+ *
+ * Cherepanov Vitaliy <v.cherepanov@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * - Samsung RnD Institute Russia
+ *
+ */
+
+#define MSG_ID_LIST \
+X(NMSG_VERSION,			0x0001) \
+X(NMSG_START,			0x0002) \
+X(NMSG_STOP,			0x0003) \
+X(NMSG_CONFIG,			0x0004) \
+X(NMSG_BINARY_INFO,		0x0005) \
+X(NMSG_GET_TARGET_INFO,		0x0007) \
+X(NMSG_SWAP_INST_ADD,		0x0008) \
+X(NMSG_SWAP_INST_REMOVE,	0x0009) \
+X(NMSG_GET_PROBE_MAP,		0x000A) \
+X(NMSG_KEEP_ALIVE,		0x000F) \
+X(NMSG_GET_SCREENSHOT,		0x0010) \
+X(NMSG_GET_PROCESS_ADD_INFO,	0x0011)
+
+#define DATA_MSG_ID_LIST \
+X(NMSG_PROCESS_INFO,		0x0001) /* target process info */ \
+X(NMSG_TERMINATE,		0x0002) /* terminate */ \
+X(NMSG_ERROR,			0x0003) /* error message */ \
+X(NMSG_SAMPLE,			0x0004) /* N	10ms */ \
+X(NMSG_SYSTEM,			0x0005) /* N	10~1000ms	DaData, start sending immediately after start message from host, first system message time is tracing start time */ \
+X(NMSG_IMAGE,			0x0006) /* N	irregular	image */ \
+X(NMSG_RECORD,			0x0007) /* N	irregular	replay event */ \
+X(NMSG_FUNCTION_ENTRY,		0x0008) /* N	irregular	swap instrumentation, Instrumented functions by AppInst and LibInst */ \
+X(NMSG_FUNCTION_EXIT,		0x0009) /* N	irregular	swap instrumentation, Instrumented functions by AppInst and LibInst */ \
+X(NMSG_CONTEXT_SWITCH_ENTRY,	0x0010) /* N	irregular	swap instrumentation for kernel */ \
+X(NMSG_CONTEXT_SWITCH_EXIT,	0x0011) /* N	irregular	swap instrumentation for kernel */ \
+
