d27dee2a5ea50ff37fdca6a4c70644681576cb35
Dmitry Kovalenko <d.kovalenko@samsung.com>
2015-06-10 07:34:37 +0300

    [FIX] modules insertion in start.sh
    add /usr/sbin/ to PATH variable
    
    Change-Id: Ibdbdc4c71ea741652a3dd95a49b3c756eeab34a7
    Signed-off-by: Dmitry Kovalenko <d.kovalenko@samsung.com>

--- daemon/start.sh
+++ daemon/start.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+PATH=$PATH:/usr/sbin/
+
 if [ ! -e /sys/kernel/debug/swap/writer/raw ]; then
 
-    insmod swap_buffer.ko || exit 1  # buffer is loaded
-    insmod swap_ksyms.ko || exit 1
-    insmod swap_driver.ko || exit 1  # driver is loaded
-    insmod swap_writer.ko || exit 1
-    insmod swap_kprobe.ko || exit 1  # kprobe is loaded
-    insmod swap_uprobe.ko || exit 1  # uprobe is loaded
-    insmod swap_us_manager.ko || exit 1  # us_manager is loaded
-    insmod swap_ks_features.ko || exit 1  # ks_features is loaded
-    insmod swap_sampler.ko || exit 1
-    insmod swap_energy.ko || exit 1
-    insmod swap_message_parser.ko || exit 1  # parser is loaded
+    insmod swap_buffer.ko           || exit 102  # buffer is loaded
+    insmod swap_ksyms.ko            || exit 103
+    insmod swap_driver.ko           || exit 104  # driver is loaded
+    insmod swap_writer.ko           || exit 105
+    insmod swap_kprobe.ko           || exit 106  # kprobe is loaded
+    insmod swap_uprobe.ko           || exit 107  # uprobe is loaded
+    insmod swap_us_manager.ko       || exit 108  # us_manager is loaded
+    insmod swap_ks_features.ko      || exit 109  # ks_features is loaded
+    insmod swap_sampler.ko          || exit 110
+    insmod swap_energy.ko           || exit 111
+    insmod swap_message_parser.ko   || exit 112  # parser is loaded
 
 fi
 
