50a7876fe71931956c1da736864f22127f6fd700
Yurchenko Darya <d.urchenko@partner.samsung.com>
2015-07-06 14:38:54 +0300

    [IMPROVE] added bluetooth consumption
    Added coefficients for calculation energy consumption
    
    Change-Id: Ieed315c81217bb52bfeb1204200c3a4887c9853e
    Signed-off-by: Yurchenko Darya <d.urchenko@partner.samsung.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,14 +14,14 @@ fi
 
 PATH=$PATH:/usr/sbin/
 
-config_file="/etc/config/model-config.xml"
-if [ -e $config_file ]; then
-	grep -i "feature/container[^>]*>[[:blank:]]*false" "$config_file" > /dev/null
-	if [ $? -ne 0 ]; then
-		echo $ERR_CONTAINER_NOT_SUPPORTED_ST
-		exit $ERR_CONTAINER_NOT_SUPPORTED
-	fi
-fi
+# config_file="/etc/config/model-config.xml"
+# if [ -e $config_file ]; then
+# 	grep -i "feature/container[^>]*>[[:blank:]]*false" "$config_file" > /dev/null
+# 	if [ $? -ne 0 ]; then
+# 		echo $ERR_CONTAINER_NOT_SUPPORTED_ST
+# 		exit $ERR_CONTAINER_NOT_SUPPORTED
+# 	fi
+# fi
 
 if [ ! -e /sys/kernel/debug/swap/enable ]; then
 
@@ -44,6 +44,7 @@ if [ ! -e /sys/kernel/debug/swap/enable ]; then
     insmod swap_preload.ko          || exit 117
     insmod swap_wsp.ko              || exit 118
     insmod swap_nsp.ko              || exit 119
+    insmod swap_ksm.ko              || exit 120
 
 fi
 
@@ -62,25 +63,52 @@ echo 1 > /sys/kernel/debug/swap/enable
 #  - they were calculated for mAs
 #  - result should be exposed in uAs
 
-# cpu idle: 62.27 / 1
-echo 62270 > /sys/kernel/debug/swap/energy/cpu_idle/numerator &&
+
+# cpu idle: 62.27 / 1 (before)
+# cpu0 running: 213.21 / 1 (before)
+# cpuN running: 97.29 / 1 (before)
+# flash read:  74.32 / 33154239 (before)
+# flash write : 141.54 / 27920983 (before)
+
+# Energy coefficients
+# result will expose in uAh
+
+#cpu idle: 1.80431805e-08
+echo 18 > /sys/kernel/debug/swap/energy/cpu_idle/numerator &&
 echo 1000000000 > /sys/kernel/debug/swap/energy/cpu_idle/denominator &&
 
-# cpu0 running: 213.21 / 1
-echo 213210 > /sys/kernel/debug/swap/energy/cpu_running/numerator &&
+# cpu0 running: 2.46948672e-08
+echo 25 > /sys/kernel/debug/swap/energy/cpu_running/numerator &&
 echo 1000000000 > /sys/kernel/debug/swap/energy/cpu_running/denominator &&
 
-# cpuN running: 97.29 / 1
-echo 97290 > /sys/kernel/debug/swap/energy/cpuN_running/numerator &&
+# cpuN running: 9.76056927e-09
+echo 10 > /sys/kernel/debug/swap/energy/cpuN_running/numerator &&
 echo 1000000000 > /sys/kernel/debug/swap/energy/cpuN_running/denominator &&
 
-# flash read:  74.32 / 33154239
-echo 74320 > /sys/kernel/debug/swap/energy/flash_read/numerator &&
-echo 33154239 > /sys/kernel/debug/swap/energy/flash_read/denominator &&
+# flash read:   2.82175507e-07
+echo 282 > /sys/kernel/debug/swap/energy/flash_read/numerator &&
+echo 1000000000 > /sys/kernel/debug/swap/energy/flash_read/denominator &&
 
-# flash write: 141.54 / 27920983
-echo 141540 > /sys/kernel/debug/swap/energy/flash_write/numerator &&
-echo 27920983 > /sys/kernel/debug/swap/energy/flash_write/denominator &&
+# flash write : 1.94010408e-06
+echo 194 > /sys/kernel/debug/swap/energy/flash_write/numerator &&
+echo 100000000 > /sys/kernel/debug/swap/energy/flash_write/denominator &&
+
+# recv through wifi : 1.01901550e-05
+echo 1019 > /sys/kernel/debug/swap/energy/wf_recv/numerator &&
+echo 100000000 > /sys/kernel/debug/swap/energy/wf_recv/denominator &&
+
+
+# send through wifi : 2.11865993e-05
+echo 2118 > /sys/kernel/debug/swap/energy/wf_send/numerator &&
+echo 100000000 > /sys/kernel/debug/swap/energy/wf_send/denominator &&
+
+# send through bluetooth : 2.53633107e-04
+echo 2536 > /sys/kernel/debug/swap/energy/hci_send_acl/numerator &&
+echo 10000000 > /sys/kernel/debug/swap/energy/hci_send_acl/system/denominator &&
+
+# recv through bluetooth : 4.08411717e-04
+echo 4084 > /sys/kernel/debug/swap/energy/l2cap_recv_acldata/numerator &&
+echo 10000000 > /sys/kernel/debug/swap/energy/l2cap_recv_acldata/denominator &&
 
 # LCD:
 if [ -d /sys/kernel/debug/swap/energy/lcd/ ]
