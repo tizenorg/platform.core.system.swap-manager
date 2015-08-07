c4d1a3887824f0b813f8a4e26f73fc591f28ff2e
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2015-08-07 13:26:27 +0300

    [DRAFT] no sudo
    Change-Id: If9bc1ef4d5cb7f2a698e6a055a24ad51dbd77381
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
 # swap disabling
-/bin/echo 0 > /sys/kernel/debug/swap/enable
+`/bin/echo 0 > /sys/kernel/debug/swap/enable`
 
-/usr/sbin/rmmod swap_nsp
-/usr/sbin/rmmod swap_wsp
-/usr/sbin/rmmod swap_preload
-/usr/sbin/rmmod swap_task_data
-/usr/sbin/rmmod swap_webprobe
-/usr/sbin/rmmod swap_fbiprobe
-/usr/sbin/rmmod swap_retprobe
-/usr/sbin/rmmod swap_message_parser
-/usr/sbin/rmmod swap_energy
-/usr/sbin/rmmod swap_sampler
-/usr/sbin/rmmod swap_ks_features
-/usr/sbin/rmmod swap_us_manager
-/usr/sbin/rmmod swap_uprobe
-/usr/sbin/rmmod swap_kprobe
-/usr/sbin/rmmod swap_writer
-/usr/sbin/rmmod swap_driver
-/usr/sbin/rmmod swap_ksyms
-/usr/sbin/rmmod swap_buffer
-/usr/sbin/rmmod swap_master
 
