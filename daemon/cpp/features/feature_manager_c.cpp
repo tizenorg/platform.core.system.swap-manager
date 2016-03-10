2bdf117480412cc429c65757624d9911eabde8dd
Vitaliy Cherepanov <v.cherepanov@samsung.com>
2016-03-10 14:27:34 +0300

    [FIX] feature support checker
    Change-Id: I608b8caa09010cfe3f956999349d0756697f4479
    Signed-off-by: Vitaliy Cherepanov <v.cherepanov@samsung.com>

--- daemon/cpp/features/feature_manager_c.cpp
+++ daemon/cpp/features/feature_manager_c.cpp
@@ -96,18 +96,17 @@ extern "C" int fm_stop(void)
     return 0;
 }
 
-static uint64_t checkSupportFeatures(uint64_t f, uint64_t fs, size_t offset)
+static uint64_t checkSupportFeatures(uint64_t setFeatures,
+                                     uint64_t supportedFeatures, size_t offset)
 {
-    if (f != fs) {
-        uint64_t diff = f ^ fs;
+    uint64_t unsupportedFeatures = setFeatures & (~supportedFeatures);
 
-        for (int i = 0; diff; diff >>= 1, ++i) {
-            if (diff & 1)
-                LOGW("feature[%d] is not support\n", i + offset);
-        }
+    for (int i = 0; unsupportedFeatures; unsupportedFeatures >>= 1, ++i) {
+        if (unsupportedFeatures & 1)
+            LOGW("feature[%d] is not support\n", i + offset);
     }
 
-    return f & fs;
+    return setFeatures & supportedFeatures;
 }
 
 extern "C" int fm_set(uint64_t f0, uint64_t f1)
