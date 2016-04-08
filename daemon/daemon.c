09ed8a86743fb6276d6cbb448508854dd495c20f
Alexander Aksenov <a.aksenov@samsung.com>
2016-04-08 14:37:05 +0300

    [FIX] Function name fix
    terminate_all_target() -> terminate_all()
    
    Change-Id: I203c6c52e6e65d4ac4f435663f6e95c529484852
    Signed-off-by: Alexander Aksenov <a.aksenov@samsung.com>

--- daemon/daemon.c
+++ daemon/daemon.c
@@ -356,7 +356,7 @@ void restart_all(void)
 
 	LOGI("Restart all profiled apps\n");
 
-	terminate_all_target();
+	terminate_all();
 	target_stop_all();
 	target_wait_all();
 	terminate_profiling_apps();
