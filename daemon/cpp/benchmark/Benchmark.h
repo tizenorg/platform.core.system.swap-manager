e3320a53da8b9696476a4cbaa3413c0cc175149c
Aleksei Mateosian <alexey.mateosyan@gmail.com>
2016-06-20 13:00:07 +0300

    SRADA-655: fixed daemon build errors
    Change-Id: I1b9f6f561225a47a7a41aaa65df23d7dd52ec4e5

--- daemon/cpp/benchmark/Benchmark.h
+++ daemon/cpp/benchmark/Benchmark.h
@@ -2,13 +2,18 @@
 #define DAEMON__BENCHMARK__BENCHMARK_H
 
 #include <inttypes.h>
+#ifdef __cplusplus
+#define EXTERN_C extern "C"
+#else 
+#define EXTERN_C
+#endif
 
 struct SystemBenchmark
 {
 	uint64_t avg_syscall_overhead;
 };
 
-extern "C" void performBenchmark(SystemBenchmark * benchmark);
+EXTERN_C void performBenchmark(struct SystemBenchmark * benchmark);
 
 
 #endif // DAEMON__BENCHMARK__BENCHMARK_H
