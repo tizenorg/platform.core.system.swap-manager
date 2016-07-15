7ee889190e8bfcec9b957d384561ef0b576d760b
Anatolii Nikulin <nikulin.a@samsung.com>
2016-07-15 09:15:42 +0300

    add missed header file
    Change-Id: Ia7d3cbb36b5d655612b5d27a138d97a77e58a119
    Signed-off-by: Anatolii Nikulin <nikulin.a@samsung.com>

---
+++ ui_viewer/ui_viewer_screenshot.h
@@ -0,0 +1,32 @@
+/*
+ *  DA manager
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact:
+ *
+ * Anatolii Nikulin <nikulin.a@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * - Samsung RnD Institute Russia
+ *
+ */
+
+#ifndef _UI_VIEWER_SCREENSHOT_
+#define _UI_VIEWER_SCREENSHOT_
+
+void wayland_deinit(void);
+
+#endif /* _UI_VIEWER_SCREENSHOT_ */
